0cd955ad9478228186828177bdfe953a040f79d8
dem4ply <dem4ply@gmail.com>
2015-06-03 01:53:06 -0500

    [website] se agregaron las platillas para el sign up

--- website/views.py
+++ website/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 def index(request):
-	template = loader.get_template('index.html')
+	template = loader.get_template('base/base.html')
+	return HttpResponse( template.render() )
+
+def user_sign_up(request):
+	template = loader.get_template('user/sign_up.html')
 	return HttpResponse( template.render() )
 
 # Create your views here.
