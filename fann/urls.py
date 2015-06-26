acfcd3f16cbaaef331c18d932545cf2ac7fdf85d
dem4ply <dem4ply@gmail.com>
2015-06-26 01:28:11 -0500

    [fann] se agrego el framework rest para django

--- fann/urls.py
+++ fann/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
    # url(r'^blog/', include('blog.urls')),
 	url(r'^', include( 'website.urls' ) ),
 	url(r'^admin/', include( admin.site.urls ) ),
-	url(r'^api/user', include( 'user.urls') ),
+	url(r'^api_auth/', include('rest_framework.urls', namespace='rest_framework')),
+	url(r'^api/v1/user', include( 'user.urls') ),
 ]
