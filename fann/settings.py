798c1382659fdb8dd6b13e89ba17b86307ea973a
dem4ply <dem4ply@gmail.com>
2015-06-26 01:43:32 -0500

    [rest] se agrego la configuracion del framework_rest

--- fann/settings.py
+++ fann/settings.py
@@ -54,6 +54,14 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
+REST_FRAMEWORK = {
+	# Use Django's standard `django.contrib.auth` permissions,
+	# or allow read-only access for unauthenticated users.
+	'DEFAULT_PERMISSION_CLASSES': [
+		'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+	]
+}
+
 ROOT_URLCONF = 'fann.urls'
 AUTH_USER_MODEL = 'user.User'
 
